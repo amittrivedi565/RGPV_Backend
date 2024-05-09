33522b4b0253369c127e1e6fda3f868cff5f6a04
amittrivedi565 <edgeparallel@gmail.com>
2024-05-10 01:32:13 +0530

    temp

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,71 +1,40 @@
-
-const db = require('../models/index');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const db = require("../models/index");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const SECRET_KEY = "VAULT";
 
 
-// User login
-
-
-
-const signInGet = async (req,res)=>
-  {
+const signInGet = async (req, res) => {
   try {
-    res.render("../views/signin")
+    res.render("../views/signin");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-const signInPost =  async (req, res) => {
-try {
-const { name, pswd } = req.body;
-const user = await db.admins.findOne({ name : name });
-if (!user) {
-return res.status(401).json({ error: 'Authentication failed' });
-}
-const passwordMatch = await bcrypt.compare(pswd, user.pswd);
-if (!passwordMatch) {
-return res.status(401).json({ error: 'Authentication failed' });
-}
-const token = jwt.sign({ userId: user._id }, SECRET_KEY, {
-expiresIn: '1h',
-});
-res.render("../views/admin")
-} catch (error) {
-res.status(500).json({ error: 'Login failed' });
-}
+
+const signInPost = async (req, res) => {
+  try {
+    const { name, pswd } = req.body;
+    const user = await db.admins.findOne({ name: name });
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+    const passwordMatch = await bcrypt.compare(pswd, user.pswd);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+    const token = jwt.sign({ userId: user._id }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+    // res.render("../views/admin")
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
+  }
 };
 
 module.exports = {
-  signInGet , signInPost
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  signInGet,
+  signInPost,
+};
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const authController = require("../controllers/auth.controller")
 
-
 router.get('/signin',authController.signInGet)
 router.post('/signin',authController.signInPost)
+
 
 module.exports = router
