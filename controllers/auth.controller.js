c033c612e4290e3b607ec14ca3595b023a3a8153
amittrivedi565 <edgeparallel@gmail.com>
2024-05-09 19:34:51 +0530

    JWT MODIFIED

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,70 +1,71 @@
-const db = require('../models/index')
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const SECRET_KEY = "NOTESAPI";
 
-const signUp = async (req, res) => {
-  const { name, pswd } = req.body;
+const db = require('../models/index');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = "VAULT";
 
-  try {
-    const existingUser = await db.admins.findOne({ name: name });
-    if (existingUser) {
-      return res.status(400).json({ message: "User Already Exists" });
-    }
 
-    const hashedPassword = await bcrypt.hash(pswd, 10);
-
-    const result = await db.admins.create({
-      name: name,
-      pswd: hashedPassword
-    });
-
-    const token = jwt.sign({ name: result.name, id: result._id }, SECRET_KEY);
-    res.status(201).json({ user: result, token: token });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something Went Wrong" });
-  }
-};
+// User login
 
 
 
-
-const signInGet = async (req,res)=>{
+const signInGet = async (req,res)=>
+  {
   try {
     res.render("../views/signin")
   } catch (error) {
-    
+    console.log(error)
   }
 }
 
-const signInPost = async (req, res) => {
-  const { name, pswd } = req.body;
-  try {
-    const existingUser = await db.admins.findOne({ name });
-
-
-    if (!existingUser) {
-      return res.status(401).json({ message: "User not found!" });
-    }
-
-    const matchPassword = await bcrypt.compare(pswd, existingUser.pswd);
-    
-      if (!matchPassword) {
-        return res.status(404).json({ message: "Invalid credentionals" });
-      }
-      const token = jwt.sign(
-        { name: existingUser.name, id: existingUser._id },
-        SECRET_KEY , {expiresIn :'.25h'}
-      );
-
-    
-      res.render("../views/admin")
-    
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something Went Wrong" });
-  }
+const signInPost =  async (req, res) => {
+try {
+const { name, pswd } = req.body;
+const user = await db.admins.findOne({ name : name });
+if (!user) {
+return res.status(401).json({ error: 'Authentication failed' });
+}
+const passwordMatch = await bcrypt.compare(pswd, user.pswd);
+if (!passwordMatch) {
+return res.status(401).json({ error: 'Authentication failed' });
+}
+const token = jwt.sign({ userId: user._id }, SECRET_KEY, {
+expiresIn: '1h',
+});
+res.render("../views/admin")
+} catch (error) {
+res.status(500).json({ error: 'Login failed' });
+}
 };
 
-module.exports = { signInGet ,signInPost , signUp };
+module.exports = {
+  signInGet , signInPost
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const authController = require("../controllers/auth.controller")
 
-router.post('/signup',authController.signUp)
 
 router.get('/signin',authController.signInGet)
 router.post('/signin',authController.signInPost)
